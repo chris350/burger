c4e295d46664813fdb1cb310bda973ac1c67d0cd
christian colon <christiancolon152@gmail.com>
2019-11-24 21:22:40 -0500

    ready for heroku

--- server.js
+++ server.js
@@ -1,26 +1,16 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-
-var PORT = process.env.PORT || 8000;
+var PORT = process.env.PORT || 8080;
 var app = express();
-
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
-app.use(bodyParser.json());
-
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-var routes = require("./controllers/burgersController.js");
-
+var routes = require("./controllers/burgers_controller");
 app.use(routes);
-
-app.listen(PORT, function() {
-  console.log("Listening on port:%s", PORT);
+app.listen(PORT, function () {
+    console.log("Listening on port:%s", PORT);
 });
